7d8e61d67bb58ca8c77137bd8e35fe5adbe85c2a
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2026-02-26 21:21:00 -0300

    Primeiro commit

---
+++ src/main/java/unicesumar/provapoolagrimadealunos/ProvaPooLagrimaDeAlunosApplication.java
@@ -0,0 +1,13 @@
+package unicesumar.provapoolagrimadealunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaPooLagrimaDeAlunosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaPooLagrimaDeAlunosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unicesumar/provapoolagrimadealunos/ProvaPooLagrimaDeAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package unicesumar.provapoolagrimadealunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPooLagrimaDeAlunosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
